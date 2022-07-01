2d5898dc249e009ff2b5ab1ef2f43400321804f2
Robert J McGinness <robertjmcginness5@gmail.com>
2022-07-01 14:32:46 -0400

    fixed directory structure

--- medguardian/medguardian/settings.py
+++ medguardian/medguardian/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7o-&e$xq$4!@9yafpkxbq$z1#-+z*cmlxghk)*=p3_14v&pqb2'
+SECRET_KEY = 'django-insecure-378!^kmuhoz6+u$4$pjuu*dp)ycr8+p^t(x2ju#@lp$-j&2&5p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
