d23814a3f58792fb60d03a2364e7c84ad09a5181
Robert J McGinness <robertjmcginness5@gmail.com>
2022-07-04 17:10:35 -0400

    updated yaml with db service

--- medguardian/medications/models.py
+++ medguardian/medications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Medication(models.Model):
+    generic_name = models.CharField(max_length=128,
+                                    help_text='The generic name of the medication')
+    brand_name = models.CharField(max_length=128,
+                                  help_text='The brand name of the medication')
+    strength = models.FloatField(help_text='The strength per dosage unit')
+    manufacturer = models.CharField(max_length=128,
+                                    help_text='The name of the manufacturer')
