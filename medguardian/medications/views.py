5c6a3cfed3cfdf5d70c812390d31e8ef4712eb3a
Robert J McGinness <robertjmcginness5@gmail.com>
2022-07-03 22:15:40 -0400

    added medication templates folder and base.html

--- medguardian/medications/views.py
+++ medguardian/medications/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    name = request.GET.get('name') or ''
+
+    return render(request, 'base.html', {'name': name})
