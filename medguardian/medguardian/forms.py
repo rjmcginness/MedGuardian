e18fa889897232acc61cfd5db6d608dc35b64c35
Robert J McGinness <robertjmcginness5@gmail.com>
2022-07-11 22:58:55 -0400

    corrected password typo

--- medguardian/medguardian/forms.py
+++ medguardian/medguardian/forms.py
@@ -3,10 +3,13 @@ from django.contrib.auth import models
 
 
 class RegistrationForm(forms.ModelForm):
-
     date_of_birth = forms.DateField(widget=forms.DateInput(
-                                        attrs={'type': 'date'})
-                                   )
+        attrs={'type': 'date'})
+    )
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_repeat = forms.CharField(label="Password (repeat)",
+                                      widget=forms.PasswordInput())
+
     class Meta:
         model = models.User
         fields = ['username', 'first_name', 'last_name', 'email']
