a16eb8507ad555afcf0ac17f9c91d328782d6566
Robert J McGinness <robertjmcginness5@gmail.com>
2022-08-01 09:22:49 -0400

    security

--- medguardian/medguardian/settings.py
+++ medguardian/medguardian/settings.py
@@ -28,11 +28,12 @@ SECRET_KEY = 'django-insecure-378!^kmuhoz6+u$4$pjuu*dp)ycr8+p^t(x2ju#@lp$-j&2&5p
 DEBUG = True
 
 ######HARDCODED FOR NOW
-ALLOWED_HOSTS = ['ec2-50-16-207-76.compute-1.amazonaws.com', 'localhost', '127.0.0.1', '0.0.0.0', '[::1]']
+ALLOWED_HOSTS = ['ec2-50-16-207-76.compute-1.amazonaws.com', 'localhost', '127.0.0.1', '0.0.0.0']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:2813',
                         'http://127.0.0.1:2813',
-                        'http://ec2-50-16-207-76.compute-1.amazonaws.com:2813',
-                        'http://ec2-50-16-207-76.compute-1.amazonaws.com:8000']
+                        'http://ec2-50-16-207-76.compute-1.amazonaws.com',
+                        'http://ec2-50-16-207-76.compute-1.amazonaws.com:8000',
+                        'http://ec2-50-16-207-76.compute-1.amazonaws.com:80']
 
 # Application definition
 
